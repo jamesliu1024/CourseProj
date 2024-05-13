2c74645f2460efdc7eef11f8238ce4a7579bcb79
jamesliu <jamesliu1024@163.com>
2024-05-13 21:40:11 +0800

    去除班级表，用户信息去除手机号，添加成绩表的课程表id字段

--- app/sampledata/init.sql
+++ app/sampledata/init.sql
@@ -7,7 +7,6 @@ create table if not exists admins
 (
     admin_id       int primary key auto_increment comment '管理员id',
     admin_name     varchar(50) comment '管理员姓名',
-    admin_phone    varchar(50) comment '管理员电话',
     admin_password varchar(50) comment '管理员密码'
 ) engine = InnoDB
   auto_increment = 1000
@@ -18,22 +17,10 @@ create table if not exists teachers
 (
     teacher_id       int primary key auto_increment comment '教师id',
     teacher_name     varchar(50) comment '教师姓名',
-    teacher_phone    varchar(50) comment '教师电话',
     teacher_password varchar(50) comment '教师密码',
     gender           int comment '性别',
     birthday         date comment '生日',
-    start_year       date comment '入职时间'
-) engine = InnoDB
-  auto_increment = 1000
-  default charset = utf8;
-
-# 班级表
-create table if not exists classes
-(
-    class_id   int primary key auto_increment comment '班级id',
-    class_name varchar(50) comment '班级名称',
-    teacher_id int comment '班主任id',
-    foreign key (teacher_id) references teachers (teacher_id)
+    start_time       date comment '入职时间'
 ) engine = InnoDB
   auto_increment = 1000
   default charset = utf8;
@@ -43,14 +30,11 @@ create table if not exists students
 (
     student_id       int primary key auto_increment comment '学生id',
     student_name     varchar(50) comment '学生姓名',
-    student_phone    varchar(50) comment '学生电话',
     student_password varchar(50) comment '学生密码',
     gender           int comment '性别',
     birthday         date comment '生日',
-    start_year       date comment '入学时间',
-    years            int comment '学年制',
-    class_id         int comment '班级',
-    foreign key (class_id) references classes (class_id)
+    start_year       int comment '入学年份',
+    years            int comment '学年制'
 ) engine = InnoDB
   auto_increment = 1000
   default charset = utf8;
@@ -62,7 +46,8 @@ create table if not exists courses
     course_name   varchar(50) comment '课程名称',
     course_credit int comment '学分',
     course_hour   int comment '学时',
-    course_week   int comment '周数'
+    course_week   int comment '周数',
+    course_type   int comment '课程类型 0:必修 1:选修'
 ) engine = InnoDB
   auto_increment = 1000
   default charset = utf8;
@@ -71,15 +56,13 @@ create table if not exists courses
 create table if not exists schedules
 (
     schedule_id  int primary key auto_increment comment '课程表id',
-    student_id   int comment '学生id',
     teacher_id   int comment '教师id',
     course_id    int comment '课程id',
-    course_day  int comment '上课时间 1-7分别代表周一到周日',
-    course_time int comment '上课时间 1-7分别代表第一节到第七节课',
+    course_day   int comment '上课时间 1-7分别代表周一到周日',
+    course_time  int comment '上课时间 1-8分别代表第一节到第八节课',
     course_place varchar(50) comment '上课地点',
     years        int comment '学年',
     terms        int comment '学期 0:上学期 1:下学期',
-    foreign key (student_id) references students (student_id),
     foreign key (teacher_id) references teachers (teacher_id),
     foreign key (course_id) references courses (course_id)
 ) engine = InnoDB
@@ -89,12 +72,22 @@ create table if not exists schedules
 # 成绩表
 create table if not exists scores
 (
-    score_id   int primary key auto_increment comment '成绩id',
-    student_id int comment '学生id',
-    course_id  int comment '课程id',
-    score      int comment '成绩',
+    score_id    int primary key auto_increment comment '成绩id',
+    student_id  int comment '学生id',
+    course_id   int comment '课程id',
+    schedule_id int comment '课程表id',
+    score       int comment '成绩',
     foreign key (student_id) references students (student_id),
-    foreign key (course_id) references courses (course_id)
+    foreign key (course_id) references courses (course_id),
+    foreign key (schedule_id) references schedules (schedule_id)
 ) engine = InnoDB
   auto_increment = 1000
   default charset = utf8;
+
+# 测试数据
+insert into admins (admin_name, admin_password) values ('admin', 'e10adc3949ba59abbe56e057f20f883eLJM');
+insert into teachers (teacher_name, teacher_password,gender,birthday,start_time) values ('teacher', 'e10adc3949ba59abbe56e057f20f883eLJM',1,'1990-01-01','2010-08-01');
+insert into students (student_name, student_password,gender,birthday,start_year,years) values ('student', 'e10adc3949ba59abbe56e057f20f883eLJM',1,'2000-10-29',2024,4);
+insert into courses (course_name, course_credit, course_hour, course_week,course_type) values ('移动终端应用开发', 3, 32, 18,0);
+insert into schedules (teacher_id, course_id, course_day, course_time, course_place, years, terms) values (1000, 1000, 4, 5, 'S104', 2023, 1);
+insert into scores (student_id, course_id, schedule_id, score) values (1000, 1000, 1000, 90);
