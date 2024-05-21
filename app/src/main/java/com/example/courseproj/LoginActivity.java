626d610d47c0b6667a1a34c37451fcffe659093d
jamesliu <jamesliu1024@163.com>
2024-05-21 14:19:02 +0800

    更改为登陆页面

---
+++ app/src/main/java/com/example/courseproj/LoginActivity.java
@@ -0,0 +1,327 @@
+package com.example.courseproj;
+
+import android.content.ContentValues;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.courseproj.DB.MD5;
+import com.example.courseproj.DB.MySQLConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+/**
+ * 登录界面
+ * 在LoginActivity中处理用户的登录
+ * 用户输入账号和密码，选择身份后，点击登录按钮
+ * 通过数据库查询，判断用户输入的账号和密码是否正确
+ * 如果用户登录成功，则将用户的登录信息存储到SharedPreferences中
+ * 跳转到相应的界面
+ */
+public class LoginActivity extends AppCompatActivity {
+    LinearLayout layout;
+    AnimationDrawable anim;
+    EditText loginAccount, loginPassword;
+    Button loginButton;
+    Spinner identitySpinner;
+    int identity;  // 0:学生 1:老师 2:管理员
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        // 将状态栏的背景颜色设置为透明
+        // 将窗口的布局参数设置为 FLAG_LAYOUT_NO_LIMITS，这将使状态栏变为透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        layout = findViewById(R.id.bgLayout);
+        loginAccount = findViewById(R.id.LoginAccount);
+        loginPassword = findViewById(R.id.LoginPassword);
+        loginButton = findViewById(R.id.LoginButton);
+        identitySpinner = findViewById(R.id.identitySpinner);
+
+        // 设置渐变动画
+        anim = (AnimationDrawable) layout.getBackground();
+        anim.setEnterFadeDuration(2000); // 设置渐入效果持续时间
+        anim.setExitFadeDuration(4000); // 设置渐出效果持续时间
+
+        // 点击密码框右侧的图标，切换密码的可见性
+        loginPassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (loginPassword.getRight() - loginPassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // your action here
+                        int visible_vertical_size = Math.round(getResources().getDimension(R.dimen.visible_vertical_size)); // 获取大小
+                        int visible_horizontal_size = Math.round(getResources().getDimension(R.dimen.visible_horizontal_size));
+                        int invisible_horizontal_size = Math.round(getResources().getDimension(R.dimen.invisible_horizontal_size));
+                        if (loginPassword.getTransformationMethod() == PasswordTransformationMethod.getInstance()) { // 如果密码框是隐藏状态
+                            Drawable visible = getResources().getDrawable(R.drawable.visible);
+                            visible = resizeDrawable(visible, visible_vertical_size, visible_horizontal_size); // 调整图标大小
+                            visible.setBounds(0, 0, visible_vertical_size, visible_horizontal_size); // 设置Drawable的边界
+                            loginPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            loginPassword.setCompoundDrawables(null, null, visible, null); // 更改图标为visible.png
+                        } else { // 如果密码框是显示状态
+                            Drawable invisible = getResources().getDrawable(R.drawable.invisible);
+                            invisible = resizeDrawable(invisible, visible_vertical_size, visible_horizontal_size); // 调整图标大小
+                            invisible.setBounds(0, 0, visible_vertical_size, invisible_horizontal_size); // 设置Drawable的边界
+                            loginPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            loginPassword.setCompoundDrawables(null, null, invisible, null); // 更改图标为invisible.png
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        // 获取identitySpinner的值，确认用户的身份是学生、老师还是管理员
+        identitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                identity = parent.getSelectedItemPosition();
+
+                // TEST 显示用户选择的身份
+//                String ii = String.valueOf(identity);
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        Toast.makeText(LoginActivity.this, ii, Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // 未选择身份
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginActivity.this, "未选择身份", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        // 点击登录按钮，处理登录
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user_id = loginAccount.getText().toString();
+                String password = loginPassword.getText().toString();
+                handleLogin(user_id, password, identity);
+            }
+        });
+    }
+
+    // 开始播放动画：在onResume方法中开始播放渐变动画
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (anim != null && !anim.isRunning())
+            anim.start();
+    }
+
+    // 停止播放动画：在onPause方法中停止播放渐变动画
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (anim != null && anim.isRunning())
+            anim.stop();
+    }
+
+    // 设置图片大小
+    private Drawable resizeDrawable(Drawable image, int width, int height) {
+        Bitmap b = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, width, height, false);
+        return new BitmapDrawable(getResources(), bitmapResized);
+    }
+
+    /**
+     * 处理登录
+     * 通过数据库查询，判断用户输入的账号和密码是否正确
+     * 如果用户登录成功，则将用户的登录信息存储到SharedPreferences中
+     * 跳转到相应的界面
+     * @param user_id 用户输入的账号
+     * @param password 用户输入的密码
+     * @param identity 0:学生 1:老师 2:管理员
+     */
+    public void handleLogin(String user_id, String password, int identity) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Connection connection = null;
+                PreparedStatement preparedStatement = null;
+                ResultSet resultSet = null;
+                try {
+                    connection = MySQLConnectionUtil.getConnection();
+                    if (connection == null) {
+                        throw new SQLException("无法连接到数据库");
+                    }
+                    String sql = "";
+                    String md5_password = MD5.md5(password);
+                    if (identity == 1) {
+                        // 老师
+                        sql = "SELECT * FROM teachers WHERE teacher_id = ? AND teacher_password = ?";
+                    } else if (identity == 2) {
+                        // 管理员
+                        sql = "SELECT * FROM admins WHERE admin_id = ? AND admin_password = ?";
+                    } else {
+                        // 学生
+                        sql = "SELECT * FROM students WHERE student_id = ? AND student_password = ?";
+                    }
+                    // TEST 显示用户输入的信息
+//                    System.out.println(identity);
+//                    System.out.println(sql);
+
+                    preparedStatement = connection.prepareStatement(sql);
+                    preparedStatement.setString(1, user_id);
+                    preparedStatement.setString(2, md5_password);
+                    resultSet = preparedStatement.executeQuery();
+                    if (resultSet.next()) {
+                        //  TEST 提示用户登录成功
+                        String login_success_test = identity + "\n" + user_id + "\n" + password + "\n" + md5_password;
+
+                        String user_name;
+                        int gender;
+                        String birthday;
+                        String start_time;
+                        int start_year;
+                        int years;
+
+                        if (identity == 1) { // 老师
+                            user_name = resultSet.getString("teacher_name");
+                            gender = resultSet.getInt("gender");
+                            birthday = resultSet.getString("birthday");
+                            start_time = resultSet.getString("start_time");
+
+                            start_year = 0;
+                            years = 0;
+                        } else if (identity == 2) { // 管理员
+                            user_name = resultSet.getString("admin_name");
+                            gender = resultSet.getInt("gender");
+                            birthday = resultSet.getString("birthday");
+                            start_time = resultSet.getString("start_time");
+
+                            start_year = 0;
+                            years = 0;
+                        } else { // 学生
+                            user_name = resultSet.getString("student_name");
+                            gender = resultSet.getInt("gender");
+                            birthday = resultSet.getString("birthday");
+                            start_year = resultSet.getInt("start_year");
+                            years = resultSet.getInt("years");
+
+                            start_time = "";
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                handleLoginSuccess(user_id, identity, user_name, gender, birthday, start_time, start_year, years);
+                                // TEST
+//                                String test_msg = user_id+ "\n" +identity+ "\n" +user_name+ "\n" +gender+ "\n" +birthday+ "\n" +start_time+ "\n" +start_year+ "\n" +years;
+//                                Toast.makeText(LoginActivity.this, test_msg, Toast.LENGTH_SHORT).show();
+
+                                // 跳转到相应的界面
+                                if (identity == 0) {
+                                    // TODO 跳转到学生界面
+                                    // Intent intent = new Intent(LoginActivity.this, StudentActivity.class);
+                                    // startActivity(intent);
+                                    Toast.makeText(LoginActivity.this, "学生界面", Toast.LENGTH_SHORT).show();
+                                } else if (identity == 1) {
+                                    // TODO 跳转到老师界面
+                                    // Intent intent = new Intent(LoginActivity.this, TeacherActivity.class);
+                                    // startActivity(intent);
+                                    Toast.makeText(LoginActivity.this, "老师界面", Toast.LENGTH_SHORT).show();
+                                } else if (identity == 2) {
+                                    // TODO 跳转到管理员界面
+                                    // Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+                                    // startActivity(intent);
+                                    Toast.makeText(LoginActivity.this, "管理员界面", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                    } else {
+                        // 用户名和密码不匹配，提示用户输入的数据错误，且清空密码框
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                loginPassword.setText("");
+                                Toast.makeText(LoginActivity.this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(LoginActivity.this, "无法连接到数据库", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } finally {
+                    // 关闭资源
+                    MySQLConnectionUtil.MySQL_DB_close(connection, preparedStatement, resultSet);
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * 处理登录成功
+     * 将用户的登录信息存储到SharedPreferences中
+     * 用于在用户登录成功后，保存用户的登录信息
+     * 保存用户的user_id和identity
+     */
+    public void handleLoginSuccess(String user_id, int identity, String user_name, int gender, String birthday, String start_time, int start_year, int years) {
+        // 创建一个SharedPreferences对象
+        SharedPreferences sharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE);
+
+        String current_time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+
+        // 使用SharedPreferences.Editor对象来存储用户的登录信息
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("user_id", user_id);
+        editor.putInt("identity", identity);
+        editor.putString("user_name", user_name);
+        editor.putInt("gender", gender);
+        editor.putString("birthday", birthday);
+        editor.putString("start_time", start_time);
+        editor.putInt("start_year", start_year);
+        editor.putInt("years", years);
+        editor.putString("login_time", current_time);
+
+        // 使用commit()或apply()方法来保存更改
+        editor.apply();
+    }
+
+
+}
