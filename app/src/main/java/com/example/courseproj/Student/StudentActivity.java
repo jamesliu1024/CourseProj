5b771a0114bac132d3d7b6361d1897b8c7ce7b3d
jamesliu <jamesliu1024@163.com>
2024-05-23 21:50:23 +0800

    添加学生页的 课表、选课、个人信息 子页

--- app/src/main/java/com/example/courseproj/Student/StudentActivity.java
+++ app/src/main/java/com/example/courseproj/Student/StudentActivity.java
@@ -3,21 +3,64 @@ package com.example.courseproj.Student;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import com.example.courseproj.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class StudentActivity extends AppCompatActivity {
     LinearLayout layout;
     AnimationDrawable anim;
 
+    private BottomNavigationView bottomNavigationView;  // 底部导航栏
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_student);
+        setContentView(R.layout.student);
 
+        initView();
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                if (item.getItemId() == R.id.navigation_courses) {
+                    selectedFragment = new CoursesFragment();
+                } else if (item.getItemId() == R.id.navigation_addCourses) {
+                    selectedFragment = new AddCoursesFragment();
+                } else if (item.getItemId() == R.id.navigation_profile) {
+                    selectedFragment = new ProfileFragment();
+                }
+
+                // 切换Fragment
+                getSupportFragmentManager().beginTransaction().replace(R.id.student_fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+
+        // 默认选择第一个Fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.student_fragment_container, new CoursesFragment()).commit();
+
+
+    }
+
+
+    /**
+     * 初始化视图
+     * 设置状态栏透明
+     * 设置渐变动画
+     */
+    private void initView() {
         // 将状态栏的背景颜色设置为透明
         // 将窗口的布局参数设置为 FLAG_LAYOUT_NO_LIMITS，这将使状态栏变为透明
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
